504a01b726f3e06658579830e467e50087776cd1
kshkurko@gmail.com <kshkurko@gmail.com@8f55a9c8-08e8-526c-a18c-af44422ab06a>
2012-06-02 08:23:59 +0000

    Cosmetic fixes with tabs/spaces

--- sim/WinCommonNixFcns.h
+++ sim/WinCommonNixFcns.h
@@ -6,19 +6,19 @@
 #if defined(WIN32) && TRAX==0
 
 #include <math.h>
-#include <stdlib.h>		// used later for ppm writer
+#include <stdlib.h>      // used later for ppm writer
 #include <stdio.h>
-#include <time.h>		// used to seed random number generator
+#include <time.h>        // used to seed random number generator
 
 // random number function
 inline float drand48() {
-	return( rand() * (1.f/RAND_MAX) );
+    return( rand() * (1.f/RAND_MAX) );
 }
 
 
 // log base 2
 inline float log2( const float &val ) {
-	return( log(val) * (1.f/log(2.0f)) );
+    return( log(val) * (1.f/log(2.0f)) );
 }
 
 
