523176c9b614f267e76121af52bc95de7151a0e7
kshkurko@gmail.com <kshkurko@gmail.com@8f55a9c8-08e8-526c-a18c-af44422ab06a>
2012-06-02 10:29:47 +0000

    Cosmetic fixes with tabs/spaces

--- llvm_trax/lib/trax_cpp.cpp
+++ llvm_trax/lib/trax_cpp.cpp
@@ -31,8 +31,8 @@
 // Windows pipe stuff for trax_cleanup (needs stdio.h)
 #ifdef WIN32
 # ifndef popen
-#	define popen _popen
-#	define pclose _pclose
+#   define popen _popen
+#   define pclose _pclose
 # endif
 #endif
 
@@ -80,25 +80,25 @@ void storef( float value, int base, int offset ) {
 int trax_getid( int value ) {
   if (value == 1) {
 #ifndef MULTIPLE_THREADS_ONE_CORE
-	pthread_t thisID = pthread_self();
-	for( unsigned int tid = 0; tid<numThreads; ++tid )
+  pthread_t thisID = pthread_self();
+  for( unsigned int tid = 0; tid<numThreads; ++tid )
 #ifdef WIN32
-		if( thisID.p==threads[tid].p )
+    if( thisID.p==threads[tid].p )
 #else
-		if( thisID==threads[tid] )
+    if( thisID==threads[tid] )
 #endif
-			return tid;
+      return tid;
 #endif
-	return 0; // threadID, if not found
+    return 0; // threadID, if not found
   } else if (value == 2) {
 #ifdef MULTIPLE_THREADS_ONE_CORE
-	  pthread_t thisID = pthread_self();
-	  for( unsigned int tid = 0; tid<numThreads; ++tid )
+    pthread_t thisID = pthread_self();
+    for( unsigned int tid = 0; tid<numThreads; ++tid )
   #ifdef WIN32
-		  if( thisID.p==threads[tid].p )
+      if( thisID.p==threads[tid].p )
   #else
-		  if( thisID==threads[tid] )
-			  return tid;
+      if( thisID==threads[tid] )
+        return tid;
   #endif
 #endif
     return 0; // coreID
@@ -170,8 +170,8 @@ int start_framebuffer;
 
 void trax_setup( runrtParams_t &opts ) {
 
-	// everything we can modify here has been set within the 
-	// parameters struct opts
+  // everything we can modify here has been set within the 
+  // parameters struct opts
 
   // setup global registers
   trax_global_registers = new FourByte[opts.num_global_registers];
@@ -184,33 +184,32 @@ void trax_setup( runrtParams_t &opts ) {
   // images as your results
   if ( opts.read_from_mem_file ) {
     // load memory from dump
-	printf( "Attempting to load memory file '%s'.\n", opts.mem_file_name.c_str() );
+	  printf( "Attempting to load memory file '%s'.\n", opts.mem_file_name.c_str() );
     float *light_pos = new float[3];
-//    int start_wq, start_scene, start_matls, start_camera;
-//    int start_bg_color, start_light, end_memory, start_permutation;
+    //int start_wq, start_scene, start_matls, start_camera;
+    //int start_bg_color, start_light, end_memory, start_permutation;
     int num_blocks;
-	std::ifstream filein( opts.mem_file_name.c_str() );
+	  std::ifstream filein( opts.mem_file_name.c_str() );
     // check for errors                                                                                                                                                                                                                                                                                                      
-    if (filein.fail())
-      { 
-	printf( "Error opening memory file '%s' for reading.\n",  opts.mem_file_name.c_str() );
-	exit(-1);
-      }
+    if (filein.fail()) { 
+      printf( "Error opening memory file '%s' for reading.\n",  opts.mem_file_name.c_str() );
+      exit(-1);
+    }
 
     filein >> num_blocks;
     trax_memory_pointer = new FourByte[num_blocks];
 
-// Original memory file organization, for Backwards compatibility
-//     filein >> start_wq ;
-//     filein >> start_framebuffer ;
-//     filein >> start_scene ;
-//     filein >> start_matls ;
-//     filein >> start_camera ;
-//     filein >> start_bg_color ;
-//     filein >> start_light ;
-//     filein >> end_memory ;
-//     filein >> light_pos[0] >> light_pos[1] >> light_pos[2] ;
-//     filein >> start_permutation ;
+    // Original memory file organization, for Backwards compatibility
+    //filein >> start_wq ;
+    //filein >> start_framebuffer ;
+    //filein >> start_scene ;
+    //filein >> start_matls ;
+    //filein >> start_camera ;
+    //filein >> start_bg_color ;
+    //filein >> start_light ;
+    //filein >> end_memory ;
+    //filein >> light_pos[0] >> light_pos[1] >> light_pos[2] ;
+    //filein >> start_permutation ;
 
     for (int i = 0; i < num_blocks; ++i) {
       filein >> trax_memory_pointer[i].ivalue;
@@ -244,20 +243,20 @@ void trax_setup( runrtParams_t &opts ) {
       bool duplicate_bvh    = false;
       bool tris_store_edges = opts.triangles_store_edges;
       
-      LoadMemory(trax_memory_pointer, bvh, MEMORY_SIZE, opts.img_width, opts.img_height,	
-		 grid_dimensions,
-		 camera, opts.model_file_name.c_str(),
-		 start_wq, start_framebuffer, start_scene,
-		 start_matls,
-		 start_camera, start_bg_color, start_light, end_memory,
-		 light_pos, start_permutation, tile_width, tile_height,
-		 ray_depth, num_samples, num_thread_procs * num_cores, num_cores,
-		 opts.subtree_size, epsilon, duplicate_bvh, tris_store_edges);
+      LoadMemory(trax_memory_pointer, bvh, MEMORY_SIZE, opts.img_width, opts.img_height, 
+                 grid_dimensions,
+                 camera, opts.model_file_name.c_str(),
+                 start_wq, start_framebuffer, start_scene,
+                 start_matls,
+                 start_camera, start_bg_color, start_light, end_memory,
+                 light_pos, start_permutation, tile_width, tile_height,
+                 ray_depth, num_samples, num_thread_procs * num_cores, num_cores,
+                 opts.subtree_size, epsilon, duplicate_bvh, tris_store_edges);
       
       
       // print out the DOT file?
       if( opts.bvh_dot_depth > 0 ) {
-	bvh->writeDOT( "bvh.dot", start_scene, trax_memory_pointer, 1, opts.bvh_dot_depth );
+        bvh->writeDOT( "bvh.dot", start_scene, trax_memory_pointer, 1, opts.bvh_dot_depth );
       }
     }
   }
@@ -300,85 +299,85 @@ void trax_cleanup( runrtParams_t &opts ){
 /// creates a bunch of threads to run rendering function
 bool trax_start_render_threads( void* (*renderFunc)(void*), const int &_numThreads ) {
 
-	// just in case
-	if( threads )
-		return false;
+  // just in case
+  if( threads )
+    return false;
 
-	numThreads			 = (_numThreads>0) ? _numThreads : 1;
-	threads				 = new pthread_t[numThreads];
-
-
-	// init mutexes
-	for( unsigned int m=0; m<numAtomicInc; ++m ) {
-		if( pthread_mutex_init( &mainMutex[m], NULL ) ) {
-			printf( "Unable to initialize mutex %d\n", m );
-			return false;
-		}
-	}
-
-	// init cond
-	if( pthread_cond_init( &mainCond, NULL ) != 0 ) {
-		printf( "Could not create conditional for barrier\n" );
-		return false;
-	}
-	mainCondInit = true;
+  numThreads  = (_numThreads>0) ? _numThreads : 1;
+  threads	  = new pthread_t[numThreads];
 
 
-	// start up the rendering...
-	for( unsigned int i=0; i<numThreads; ++i ) {
-		if( pthread_create( &threads[i], NULL, renderFunc, 0 ) ) {
-			printf( "Could not create thread %d\n", i );
-			return false;
-		}
-	}
+  // init mutexes
+  for( unsigned int m=0; m<numAtomicInc; ++m ) {
+    if( pthread_mutex_init( &mainMutex[m], NULL ) ) {
+      printf( "Unable to initialize mutex %d\n", m );
+      return false;
+    }
+  }
+
+  // init cond
+  if( pthread_cond_init( &mainCond, NULL ) != 0 ) {
+    printf( "Could not create conditional for barrier\n" );
+    return false;
+  }
+  mainCondInit = true;
 
 
-	// make sure all the threads exit upon completion
-	for( unsigned int i=0; i<numThreads; ++i ) {
-		if( pthread_join( threads[i], NULL ) ) {
-			printf( "Could not join thread %d\n", i );
-			return false;
-		}
-	}
+  // start up the rendering...
+  for( unsigned int i=0; i<numThreads; ++i ) {
+    if( pthread_create( &threads[i], NULL, renderFunc, 0 ) ) {
+      printf( "Could not create thread %d\n", i );
+      return false;
+    }
+  }
 
-	// clear out the memory
-	delete[] threads;
-	threads = 0;
-	for( unsigned int m=0; m<numAtomicInc; ++m )
-		pthread_mutex_destroy( &mainMutex[m] );
-	pthread_cond_destroy( &mainCond );
-	mainCondInit = false;
-	return true;
+
+  // make sure all the threads exit upon completion
+  for( unsigned int i=0; i<numThreads; ++i ) {
+    if( pthread_join( threads[i], NULL ) ) {
+      printf( "Could not join thread %d\n", i );
+      return false;
+    }
+  }
+
+  // clear out the memory
+  delete[] threads;
+  threads = 0;
+  for( unsigned int m=0; m<numAtomicInc; ++m )
+    pthread_mutex_destroy( &mainMutex[m] );
+  pthread_cond_destroy( &mainCond );
+  mainCondInit = false;
+  return true;
 }
 
 
 // main function
 int main( int argc, char* argv[] ) {
 
-	// parse some input, like number of processors, etc
-	runrtParams_t opts;
-	programOptParser( opts, argc, argv );
+  // parse some input, like number of processors, etc
+  runrtParams_t opts;
+  programOptParser( opts, argc, argv );
 
-	trax_setup( opts );
+  trax_setup( opts );
 
-	if( trax_start_render_threads( &trax_mainPThreads, opts.num_render_threads ) == false ) {
-		std::cout << "No threads available for rendering -> Ray Queues won't work!\n";
+  if( trax_start_render_threads( &trax_mainPThreads, opts.num_render_threads ) == false ) {
+    std::cout << "No threads available for rendering -> Ray Queues won't work!\n";
 
-		// time to perform one centralized cleanup :D
-		delete[] threads;
-		threads = 0;
-		for( unsigned int m=0; m<numAtomicInc; ++m )
-			pthread_mutex_destroy( &mainMutex[m] );
-		pthread_cond_destroy( &mainCond );
-		mainCondInit = false;
+    // time to perform one centralized cleanup :D
+    delete[] threads;
+    threads = 0;
+    for( unsigned int m=0; m<numAtomicInc; ++m )
+      pthread_mutex_destroy( &mainMutex[m] );
+    pthread_cond_destroy( &mainCond );
+    mainCondInit = false;
 
-		// do single threaded execution
-		std::cout << "Executing single threaded!\n";
-		trax_main();
-	}
+    // do single threaded execution
+    std::cout << "Executing single threaded!\n";
+    trax_main();
+  }
 
-	trax_cleanup( opts );
-	return 0;
+  trax_cleanup( opts );
+  return 0;
 }
 
 
@@ -388,118 +387,118 @@ int main( int argc, char* argv[] ) {
 // prints help menu
 void printHelp( char* progName ) {
 
-	// for defaults
-	runrtParams_t def;
+  // for defaults
+  runrtParams_t def;
 
-	printf( "Usage for: %s\n", progName );
-	printf( "\t--help              [prints help menu]\n" );
-	printf( "\t--custom-mem-loader <custom loader to use -- default: %d (off)\n", def.custom_mem_loader );
-	printf( "\t--height            <height in pixels -- default: %d>\n", def.img_height );
-	printf( "\t--light-file        <light file name -- default: %s>\n", def.light_file_name.c_str() );
-	printf( "\t--mem-file          <memory file to load -- default: %s>\n", def.mem_file_name.c_str() );
-	printf( "\t--model             <model file name (.obj) -- default: %s>\n", def.model_file_name.c_str() );
-	printf( "\t--num-cores         <number of render threads -- default: %d>\n", def.num_render_threads );
-	printf( "\t--num-globals       <number of global registers -- default %d>\n", def.num_global_registers );
-	printf( "\t--num-samples       <number of samples per pixel -- default: %d>\n", def.num_samples_per_pixel );
-	printf( "\t--ray-depth         <depth of rays -- default: %d>\n", def.ray_depth );
-	printf( "\t--subtree-size      <minimum size in words of BVH subtrees -- default: %d (won't build subtrees)>\n", def.subtree_size );
-	printf( "\t--triangles-store-edges [set flag to store 2 edge vecs in a tri instead of 2 verts -- default: off]\n" );
-	printf( "\t--view-file         <view file name -- default: %s>\n", def.view_file_name.c_str() );
-	printf( "\t--width             <width in pixels -- default: %d>\n", def.img_width );
-	printf( "\t--write-dot         <depth>  generates dot files for the tree after each frame. Depth should not exceed 8\n");
+  printf( "Usage for: %s\n", progName );
+  printf( "\t--help              [prints help menu]\n" );
+  printf( "\t--custom-mem-loader <custom loader to use -- default: %d (off)\n", def.custom_mem_loader );
+  printf( "\t--height            <height in pixels -- default: %d>\n", def.img_height );
+  printf( "\t--light-file        <light file name -- default: %s>\n", def.light_file_name.c_str() );
+  printf( "\t--mem-file          <memory file to load -- default: %s>\n", def.mem_file_name.c_str() );
+  printf( "\t--model             <model file name (.obj) -- default: %s>\n", def.model_file_name.c_str() );
+  printf( "\t--num-cores         <number of render threads -- default: %d>\n", def.num_render_threads );
+  printf( "\t--num-globals       <number of global registers -- default %d>\n", def.num_global_registers );
+  printf( "\t--num-samples       <number of samples per pixel -- default: %d>\n", def.num_samples_per_pixel );
+  printf( "\t--ray-depth         <depth of rays -- default: %d>\n", def.ray_depth );
+  printf( "\t--subtree-size      <minimum size in words of BVH subtrees -- default: %d (won't build subtrees)>\n", def.subtree_size );
+  printf( "\t--triangles-store-edges [set flag to store 2 edge vecs in a tri instead of 2 verts -- default: off]\n" );
+  printf( "\t--view-file         <view file name -- default: %s>\n", def.view_file_name.c_str() );
+  printf( "\t--width             <width in pixels -- default: %d>\n", def.img_width );
+  printf( "\t--write-dot         <depth>  generates dot files for the tree after each frame. Depth should not exceed 8\n");
 }
 
 
 // options parser
 void programOptParser( runrtParams_t &params, int argc, char* argv[] ) {
 
-	for( int i=1; i<argc; ++i ) {
-		printf( "%s ", argv[i] );
-		int argnum = i;
+  for( int i=1; i<argc; ++i ) {
+    printf( "%s ", argv[i] );
+    int argnum = i;
 
-		// print help
-		if( strcmp(argv[i], "--help")==0 ) {
-			printHelp( argv[0] );
-			exit( 0 );
-		}
+    // print help
+    if( strcmp(argv[i], "--help")==0 ) {
+      printHelp( argv[0] );
+      exit( 0 );
+    }
 
-		// custom memory loader?
-		else if( strcmp(argv[i], "--custom-mem-loader")==0 ) {
-			params.custom_mem_loader	= atoi( argv[++i] );
-		}
+    // custom memory loader?
+    else if( strcmp(argv[i], "--custom-mem-loader")==0 ) {
+      params.custom_mem_loader     = atoi( argv[++i] );
+    }
 
-		// image height
-		else if( strcmp(argv[i], "--height")==0 ) {
-			params.img_height			= atoi( argv[++i] );
-		}
+    // image height
+    else if( strcmp(argv[i], "--height")==0 ) {
+      params.img_height            = atoi( argv[++i] );
+    }
 
-		// light file
-		else if( strcmp(argv[i], "--light-file")==0 ) {
-			params.light_file_name		= argv[++i];
-		}
+    // light file
+    else if( strcmp(argv[i], "--light-file")==0 ) {
+      params.light_file_name       = argv[++i];
+    }
 
-		// read in memory file?
-		else if( strcmp(argv[i], "--mem-file")==0 ) {
-			params.read_from_mem_file	= true;
-			params.mem_file_name		= argv[++i];
-		}
+    // read in memory file?
+    else if( strcmp(argv[i], "--mem-file")==0 ) {
+      params.read_from_mem_file    = true;
+      params.mem_file_name         = argv[++i];
+    }
 
-		// model file name
-		else if( strcmp(argv[i], "--model")==0 ) {
-			params.model_file_name		= argv[++i];
-		}
+    // model file name
+    else if( strcmp(argv[i], "--model")==0 ) {
+      params.model_file_name       = argv[++i];
+    }
 
-		// set number of cores for rendering
-		else if( strcmp(argv[i], "--num-cores")==0 ) {
-			params.num_render_threads	= atoi( argv[++i] );
-		}
+    // set number of cores for rendering
+    else if( strcmp(argv[i], "--num-cores")==0 ) {
+      params.num_render_threads    = atoi( argv[++i] );
+    }
 
-		// number of global registers
-		else if( strcmp(argv[i], "--num-globals")==0 ) {
-			params.num_global_registers = atoi( argv[++i] );
-		}
+    // number of global registers
+    else if( strcmp(argv[i], "--num-globals")==0 ) {
+      params.num_global_registers  = atoi( argv[++i] );
+    }
 
-		// number of samples per pixel
-		else if( strcmp(argv[i], "--num-samples")==0 ) {
-			params.num_samples_per_pixel= atoi( argv[++i] );
-		}
+    // number of samples per pixel
+    else if( strcmp(argv[i], "--num-samples")==0 ) {
+      params.num_samples_per_pixel = atoi( argv[++i] );
+    }
 
-		// ray depth for traversal
-		else if( strcmp(argv[i], "--ray-depth")==0 ) {
-			params.ray_depth			= atoi( argv[++i] );
-		}
+    // ray depth for traversal
+    else if( strcmp(argv[i], "--ray-depth")==0 ) {
+      params.ray_depth             = atoi( argv[++i] );
+    }
 
-		// store edges instead of points for triangle?
-		else if( strcmp(argv[i], "--triangles-store-edges")==0 ) {
-			params.triangles_store_edges= true;
-		}
+    // store edges instead of points for triangle?
+    else if( strcmp(argv[i], "--triangles-store-edges")==0 ) {
+      params.triangles_store_edges = true;
+    }
 
-		// view file
-		else if( strcmp(argv[i], "--view-file")==0 ) {
-			params.view_file_name		= argv[ ++i ];
-		}
+    // view file
+    else if( strcmp(argv[i], "--view-file")==0 ) {
+      params.view_file_name        = argv[ ++i ];
+    }
 
-		// image width
-		else if( strcmp(argv[i], "--width")==0 ) {
-			params.img_width			= atoi( argv[++i] );
-		}
+    // image width
+    else if( strcmp(argv[i], "--width")==0 ) {
+      params.img_width             = atoi( argv[++i] );
+    }
 
-		// write dot file for BVH
-		else if( strcmp(argv[i], "--write-dot")==0 ) {
-			params.bvh_dot_depth		= atoi( argv[++i] );
-		}
+    // write dot file for BVH
+    else if( strcmp(argv[i], "--write-dot")==0 ) {
+      params.bvh_dot_depth         = atoi( argv[++i] );
+    }
 
-		// badness - help?
-		else {
-			printf( "Unrecognized option %s\n", argv[i] );
-			printHelp( argv[0] );
-			exit( -1 );
-		}
+    // badness - help?
+    else {
+      printf( "Unrecognized option %s\n", argv[i] );
+      printHelp( argv[0] );
+      exit( -1 );
+    }
 
-		if( argnum!=i )
-			printf( " %s", argv[i] );
-		printf( "\n" );
-	}
+    if( argnum!=i )
+      printf( " %s", argv[i] );
+    printf( "\n" );
+  }
 }
 
 
