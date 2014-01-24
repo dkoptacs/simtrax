eb8146589dc98a42c227eecb91eae38688aea712
dkopta <dkopta@8f55a9c8-08e8-526c-a18c-af44422ab06a>
2014-01-24 09:28:40 +0000

    recompiled memcpy should be faster

--- llvm_trax/lib/memcpy.s
+++ llvm_trax/lib/memcpy.s
@@ -7,31 +7,22 @@
 memcpy:
 	.frame	r1,0,r15
 	.mask	0x0
-# BB#0:                                 # %entry
-	ADD       r3, r0, r0
-	CMP       r3, r3, r8
-	beqid     r3, ($BB0_3)
+# BB#0:
+	beqid     r7, ($BB0_3)
 	NOP    
-# BB#1:                                 # %bb.lr.ph
-	ADD       r3, r0, r0
-	ADD       r4, r6, r0
-	ADD       r10, r0, r0
-$BB0_2:                                 # %bb
+# BB#1:
+	ADD      r3, r0, r0
+$BB0_2:                                 # %.lr.ph
                                         # =>This Inner Loop Header: Depth=1
-	ADDI      r5, r0, -1
-	ADDK      r8, r8, r5
-	ADDKC      r3, r3, r5
-	lbui      r5, r7, 0
-	ADDI      r9, r4, 1
-	ADDI      r7, r7, 1
-	BITOR     r11, r8, r3
-	sbi       r5, r4, 0
-	CMP       r5, r10, r11
-	bneid     r5, ($BB0_2)
-	ADD       r4, r9, r0
-$BB0_3:                                 # %return
+	lbu       r4, r6, r3
+	sb        r4, r5, r3
+	ADDI      r3, r3, 1
+	CMPU      r4, r7, r3
+	bltid     r4, ($BB0_2)
+	NOP    
+$BB0_3:                                 # %.loopexit
 	rtsd      r15, 8
-	ADD       r3, r6, r0
+	ADD      r3, r5, r0
 	.end	_Z6memcpyPvPKvj
 $tmp0:
 	.size	_Z6memcpyPvPKvj, ($tmp0)-_Z6memcpyPvPKvj
