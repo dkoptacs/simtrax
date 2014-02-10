10dbe5244789c33ea9007a6f4bc4882e084ebc10
dkopta <dkopta@8f55a9c8-08e8-526c-a18c-af44422ab06a>
2014-02-10 03:16:05 +0000

    recompiled memset using new compiler. Old one seemed to use different calling convention

--- llvm_trax/lib/memset.s
+++ llvm_trax/lib/memset.s
@@ -1,50 +1,44 @@
-	.file	"memset.bc"
+	.file	"rt.bc"
 	.text
 	.globl	_Z6memsetPvij
 	.align	2
 	.type	_Z6memsetPvij,@function
 	.ent	_Z6memsetPvij           # @_Z6memsetPvij
 memset:
-	.frame	r1,16,r15
+	.frame	r1,12,r15
 	.mask	0x0
-# BB#0:                                 # %entry
-	ADDI      r1, r1, -16
-	SWI       r6, r1, 24
-	SWI       r7, r1, 28
-	SWI       r8, r1, 32
-	brid      ($BB0_2)
-	LWI       r3, r1, 24
-$BB0_1:                                 # %bb
-                                        #   in Loop: Header=BB0_2 Depth=1
-	LWI       r3, r1, 28
-	LWI       r4, r1, 12
-	sbi       r3, r4, 0
-	LWI       r3, r1, 12
-	ADDI      r3, r3, 1
-$BB0_2:                                 # %bb
-                                        # =>This Inner Loop Header: Depth=1
-	SWI       r3, r1, 12
-	LWI       r3, r1, 32
-	ADD       r4, r0, r0
-	CMP       r4, r4, r3
-	bneid     r4, ($BB0_4)
-	ADDI      r5, r0, 1
-# BB#3:                                 # %bb1
-                                        #   in Loop: Header=BB0_2 Depth=1
-	ADDI      r5, r0, 0
-$BB0_4:                                 # %bb1
-                                        #   in Loop: Header=BB0_2 Depth=1
-	ADDI      r3, r3, -1
-	SWI       r3, r1, 32
-	bneid     r4, ($BB0_1)
-	sbi       r5, r1, 8
-# BB#5:                                 # %bb2
-	LWI       r3, r1, 24
+# BB#0:
+	ADDI      r1, r1, -12
+	SWI       r5, r1, 16
+	SWI       r6, r1, 20
+	SWI       r7, r1, 24
+	beqid     r7, ($BB0_4)
+	NOP    
+# BB#1:
+	LWI       r3, r1, 16
 	SWI       r3, r1, 4
+	brid      ($BB0_2)
+	SWI       r0, r1, 8
+$BB0_3:                                 #   in Loop: Header=BB0_2 Depth=1
+	LWI       r3, r1, 4
+	LWI       r4, r1, 8
+	LWI       r5, r1, 20
+	sb        r5, r3, r4
+	LWI       r3, r1, 8
+	ADDI      r3, r3, 1
+	SWI       r3, r1, 8
+$BB0_2:                                 # =>This Inner Loop Header: Depth=1
+	LWI       r3, r1, 8
+	LWI       r4, r1, 24
+	CMPU      r3, r4, r3
+	bltid     r3, ($BB0_3)
+	NOP    
+$BB0_4:
+	LWI       r3, r1, 16
 	SWI       r3, r1, 0
 	LWI       r3, r1, 0
 	rtsd      r15, 8
-	ADDI      r1, r1, 16
+	ADDI      r1, r1, 12
 	.end	_Z6memsetPvij
 $tmp0:
 	.size	_Z6memsetPvij, ($tmp0)-_Z6memsetPvij
