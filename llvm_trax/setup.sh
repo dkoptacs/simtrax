4778df2a94e01fc44242f1af5aa21b3eabf83702
Daniel Kopta <dkopta@gmail.com>
2015-01-04 22:39:09 -0700

    -Updating llvm setup script to use llvm-3.5 -Removing llvm-3.1 tarball (script now downloads appropriate files)
    Former-commit-id: d826f72d8e3bc1738a9c4ad9dfe13be69fed3612

--- llvm_trax/setup.sh
+++ llvm_trax/setup.sh
@@ -7,26 +7,88 @@ echo Setting up llvm-trax workspace
 NUM_CORES=4
 
 PWD=`pwd`
+#http://llvm.org/releases/3.5.0/clang+llvm-3.5.0-macosx-apple-darwin.tar.xz
 
-echo Extracting llvm-3.1...
-tar zxf llvm-3.1.tar.gz
 
-LLVMDIR=$PWD/llvm-3.1
-
-# Configure and build
-cd $LLVMDIR
-echo Configuring...
-./configure
-echo Building...
-make -j$NUM_CORES
-cd ../
-
-# Create symbolic link if it doesn't exist yet
-if [ -L bin ]; then
-    echo bin symbolic link already exists.
-else
-    echo Creating symbolic link to bin directory...
-    ln -s $LLVMDIR/Release+Asserts/bin bin
+which wget &> /dev/null
+if [ $? == 0 ]; then
+    DOWNLOADER="wget"
+    DOWNLOADEROPTS = "-O"
+fi
+#echo $DOWNLOADER
+if [ ! -n "$DOWNLOADER" ]; then
+    which curl &> /dev/null
+    if [ $? == 0 ]; then
+	DOWNLOADER="curl"
+	DOWNLOADEROPTS="-o"
+    else
+	echo setup needs curl or wget
+	exit 1
+    fi
 fi
 
-echo To rebuild, enter $LLVMDIR and run \"make clean\", then delete $PWD/bin then run this script again.
+uname | grep 'Darwin' &> /dev/null
+if [ $? == 0 ]; then
+   CLANGURL="http://llvm.org/releases/3.5.0/clang+llvm-3.5.0-macosx-apple-darwin.tar.xz"
+   CLANGDIR="clang+llvm-3.5.0-macosx-apple-darwin"
+fi
+uname | grep 'FreeBSD' &> /dev/null
+if [ $? == 0 ]; then
+   CLANGURL="http://llvm.org/releases/3.5.0/clang+llvm-3.5.0-amd64-unknown-freebsd10.tar.xz"
+   CLANGDIR="clang+llvm-3.5.0-amd64-unknown-freebsd10"
+fi
+uname | grep 'Linux' &> /dev/null
+if [ $? == 0 ]; then
+    cat /proc/version | grep 'Red'
+    if [ $? == 0 ]; then
+	CLANGURL="http://llvm.org/releases/3.5.0/clang+llvm-3.5.0-i686-fedora20.tar.xz"
+	CLANGDIR="clang+llvm-3.5.0-i686-fedora20"
+    fi
+    cat /proc/version | grep 'Ubuntu'
+    if [ $? == 0 ]; then
+	CLANGURL="http://llvm.org/releases/3.5.0/clang+llvm-3.5.0-x86_64-linux-gnu-ubuntu-14.04.tar.xz"
+	CLANGDIR="clang+llvm-3.5.0-x86_64-linux-gnu-ubuntu-14.04"
+    fi
+    cat /proc/version | grep 'SUSE'
+    if [ $? == 0 ]; then
+	CLANGURL="http://llvm.org/releases/3.5.0/clang+llvm-3.5.0-x86_64-opensuse13.1.tar.xz"
+	CLANGDIR="clang+llvm-3.5.0-x86_64-opensuse13.1"
+    fi
+fi
+
+echo Downloading clang...
+RUN="$DOWNLOADER $DOWNLOADEROPTS clang.tar.xz $CLANGURL"
+echo $RUN
+$RUN
+
+echo Extracting clang...
+tar zxf clang.tar.xz
+RUN="mv $CLANGDIR clang-3.5.0"
+$RUN
+
+echo Downloading llvm...
+RUN="$DOWNLOADER $DOWNLOADEROPTS llvm-3.5.0.src.tar.xz http://llvm.org/releases/3.5.0/llvm-3.5.0.src.tar.xz"
+echo $RUN
+$RUN
+
+echo Extracting llvm...
+tar zxf llvm-3.5.0.src.tar.xz
+
+echo Copying TRaX-specific files to llvm-3.5.0.src...
+cp Trax/IntrinsicsMips.td llvm-3.5.0.src/include/llvm/IR/IntrinsicsMips.td
+cp Trax/MipsInstrFormats.td llvm-3.5.0.src/lib/Target/Mips/MipsInstrFormats.td
+cp Trax/MipsSchedule.td llvm-3.5.0.src/lib/Target/Mips/MipsSchedule.td
+cp Trax/MipsInstrInfo.td llvm-3.5.0.src/lib/Target/Mips/MipsInstrInfo.td
+
+CLANGBIN="$PWD/clang-3.5.0/bin/clang++"
+
+cd llvm-3.5.0.src
+echo Configuring...
+CXX=$CLANGBIN ./configure
+
+echo Building...
+CXX=$CLANGBIN make -j$NUM_CORES
+
+
+
+
