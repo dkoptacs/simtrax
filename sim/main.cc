2a1c32a7816ff81f990f1dc88048b9b440744f15
Daniel Kopta <dkopta@gmail.com>
2015-02-01 01:16:05 -0700

    Adding movt_s to instruction recognition exception list. Somehow this didn't make it in to my last commit.
    Former-commit-id: 4dc5aae71bfc61582b64df514b466156d86f63a1

--- sim/main.cc
+++ sim/main.cc
@@ -912,6 +912,7 @@ int main(int argc, char* argv[]) {
        instructions[i]->op == Instruction::move ||
        instructions[i]->op == Instruction::movn ||
        instructions[i]->op == Instruction::movt ||
+       instructions[i]->op == Instruction::movt_s ||
        instructions[i]->op == Instruction::mov_s ||
        instructions[i]->op == Instruction::movn_s ||
        instructions[i]->op == Instruction::movz_s ||
