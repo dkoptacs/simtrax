a1f23e5f52f2a605be9780605b92baa1e76eece9
Daniel Kopta <dkopta@gmail.com>
2015-03-07 22:25:10 -0700

    -Adding rebuild.sh script for llvm_trax, so that compiler modifications do not require a full setup.
    Former-commit-id: a1b58e75e2f70c6230bf8b66a3466f272ac01ff4

---
+++ llvm_trax/rebuild.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This script rebuilds the llvm-trax compiler.
+
+NUM_CORES=3
+
+echo Copying TRaX-specific files to llvm-3.5.0.src...
+cp Trax/IntrinsicsMips.td llvm-3.5.0.src/include/llvm/IR/IntrinsicsMips.td
+cp Trax/MipsInstrFormats.td llvm-3.5.0.src/lib/Target/Mips/MipsInstrFormats.td
+cp Trax/MipsSchedule.td llvm-3.5.0.src/lib/Target/Mips/MipsSchedule.td
+cp Trax/MipsInstrInfo.td llvm-3.5.0.src/lib/Target/Mips/MipsInstrInfo.td
+
+cd build
+
+echo Building...
+make -j$NUM_CORES
